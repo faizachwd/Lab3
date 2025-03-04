ff368bcd01b26c38c1f70ef09fbdfe734483bcf1
faizachwd <faiza.chowdhury789@gmail.com>
2025-03-04 17:12:11 -0500

    final map

--- script.js
+++ script.js
@@ -5,39 +5,76 @@ const map = new mapboxgl.Map({
     container: "my-map",
     style: 'mapbox://styles/faiza132/cm72f283a007a01quayc2g04v', // basemap, monochrome style to make data points pop
     center: [-79.3878583, 43.7205208], //Toronto's centre lat and lon
-    zoom: 12
+    zoom: 10.5
 });
 
-// /*--------------------------------------------------------------------
-// ADD DATA AS CHOROPLETH MAP ON MAP LOAD
-// Use get expression to categorise data based on population values
-// Same colours and threshold values are used in hardcoded legend
-// --------------------------------------------------------------------*/
+map.addControl(new mapboxgl.NavigationControl());
+
 // // Add data source and draw initial visiualization of layer
 map.on('load', () => {
     map.addSource('fire-incident-data', {
         'type': 'vector',
-        'url': "mapbox://faiza132.at4sfay1"
+        'url': "mapbox://faiza132.akmo1cn8"
     });
+    //Layer with symbology
     map.addLayer({
         'id': 'fire-points',
         'type': 'circle',
         'source': 'fire-incident-data',
+        'minzoom': 0,
+        'maxzoom': 24,
         'paint': {
             'circle-color': [
                 'step',
                 ['get', 'Event_Alarm_Level'], // GET expression retrieves property value from 'capacity' data field
-                '#FFD407', // Colour assigned to any values < first step
-                1, '#FFB536', // Colours assigned to values >= each step
-                2, '#FC9841',
-                3, '#F06832',
-                4, '#E23D28',
-                5, '#CE2029',
-                6, '#9F191F'
+                '#ffe74d', // Colour assigned to any values < first step
+                2, "#f77f00",
+                3, '#fc7091',
+                4, '#d62828',
+                5, '#7c3f81',
+                6, '#004468'
             ],
-            'circle-opacity': 0.5,
-            'circle-stroke-color': 'black'
+            'circle-opacity': 0.9,
+            'circle-stroke-color': 'black',
+            'circle-radius': 6
         },
-        'source-layer': "Basic_incidents_details_2014_-clr2re"
+        'source-layer': "fire_2014-3rmicb"
+
     });
 });
+
+// Popup on hover
+//declaring popup outside of mouseenter so that i can remove it using mouseleave
+let popup = new mapboxgl.Popup()
+
+
+map.on('mouseenter', 'fire-points', (e) => {
+    //pointer so users know which point they are hovering over because many of them overlap
+    map.getCanvas().style.cursor = 'pointer';
+    popup
+        .setLngLat(e.lngLat)
+        .setHTML("<b>Fire Incident:</b><br>" +
+            "Persons Rescued: " + e.features[0].properties.Persons_Rescued)
+                .addTo(map);
+});
+
+//popup remove
+map.on('mouseleave', 'fire-points', () => {
+    popup.remove();
+});
+
+// Method that changes visualization of a layer based on an event: upon clicking a point it hides all other points that do not have the same event alarm level
+map.on('click', 'fire-points', (e) => {
+    if (e.features.length > 0) {
+        // in the second argument the setFilter function will filter out for all points that have the same alert level as the clicked on point
+        map.setFilter('fire-points', ['==', ['get', 'Event_Alarm_Level'], e.features[0].properties.Event_Alarm_Level]);
+
+    }
+});
+
+// undo's the fitler of points
+document.getElementById('reset-btn').addEventListener('click', () => {
+    // second arugment of setFilter takes a filer, and since i want all filters removed its set to null
+    map.setFilter('fire-points', null);
+});
+
